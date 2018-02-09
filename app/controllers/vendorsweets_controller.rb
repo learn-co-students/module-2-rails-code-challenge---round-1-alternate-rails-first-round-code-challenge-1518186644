687d12b9b6f6c904bdc7212222b99faf97e05e64
tanelam <tania.e.aparicio@gmail.com>
2018-02-09 11:24:19 -0500

    Done.

--- app/controllers/vendorsweets_controller.rb
+++ app/controllers/vendorsweets_controller.rb
@@ -2,11 +2,13 @@ class VendorsweetsController < ApplicationController
 
   def new
     @vendorsweet = VendorSweet.new
+    @sweets = Sweet.all
+    @vendors = Vendor.all
   end
 
   def create
     @vendorsweet = VendorSweet.create(vendorsweet_params)
-    @sweets = Sweet.all
+
   end
 
   private
